9be5e3b399e0fc136a6b57f29aab7a67527cc63a
Day <51234957+deyenelira@users.noreply.github.com>
2023-11-11 12:29:48 -0300

    add 1.java

---
+++ condicionais/1.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+//  Faça um programa que receba dois números e mostre o maior deles. Caso eles sejam iguais, deve-se mostrar a mensagem "Os números são iguais".
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    double a = in.nextInt();
+    double b = in.nextInt();
+
+    if (a > b) {
+      System.out.println(a);
+    } else {
+      if (a == b) {
+        System.out.println("Os números são iguais");
+      } else {
+        System.out.println(b);
+      }
+    }
+  }
+}
